ec122bc64d70a4dc98b0edf63ba5195c3cb5f26d
Chris Perkins <chrisperkins99@gmail.com>
2012-06-03 18:08:36 -0400

    Added basic tests for all the standard hands.
    Also started to implement matchers for some of the hands. Most tests are red right now.

--- poker/src/poker/core.clj
+++ poker/src/poker/core.clj
@@ -2,12 +2,12 @@
   (:require [clojure [string :as s]]))
 
 (defn parse-card
-  "Parse a string such as "
+  "Parse a string such as \"3h\" or \"Ks\" as a card. "
   [s]
   (if-let [[_ value suit] (re-matches #"([AKQJ2-9]|10)([cdhs])" s)]
-    [(keyword suit)
-     (or (get {"A" 14 "K" 13 "Q" 12 "J" 11} value)
-         (Integer/parseInt value))]
+    {:suit (keyword suit)
+     :value (or (get {"A" 14 "K" 13 "Q" 12 "J" 11} value)
+                (Integer/parseInt value))}
     (throw (IllegalArgumentException. (str "Illegal card: " s)))))
 
 (defn parse-hand
@@ -20,3 +20,112 @@
        (map parse-card)
        (into [])))
 
+
+(def
+  ^{:dynamic true
+    :doc "Assigns a numeric value to each poker hand.
+          Actual values are arbitrary - they are only meaningful relative
+          to each other."}
+  *rankings*
+  {:straight-flush 100
+   :four-of-a-kind 90
+   :full-house 80
+   :flush 70
+   :straight 60
+   :three-of-a-kind 50
+   :two-pair 40
+   :one-pair 30
+   :high-card 20
+   })
+
+
+(defn ranked-groups
+  "Return ranked list of pairs, triples, etc.
+   Example: (ranked-groups (parse-hand \"5s Ah 5c Kh 5d\")) =>
+            [[3 5] [1 14] [1 13]] "
+  [hand]
+  (->> hand
+       (map :value)
+       frequencies
+       (map (fn [[val count]] [count val]))
+       sort
+       reverse))
+
+(defn- match-n-of-a-kind
+  [n hand]
+  (->> hand
+       ranked-groups
+       (filter (fn [[count val]] (= n count)))))
+
+
+(defn- match-straight-impl [hand]
+  (let [vals (sort (map :value hand))
+        diffs (for [[a b] (partition 2 1 vals)]
+                (- b a))]
+    (if (every? #(= 1 %) diffs)
+      [(*rankings* :straight) (last vals)])))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Matchers for actual poker hands, in ascending
+;; order of value.
+
+(defn match-high-card [hand]
+  ())
+
+(defn match-pair [hand]
+  nil)
+
+(defn match-two-pair [hand]
+  nil)
+
+(defn match-three-of-a-kind [hand]
+  (if-let [[_ v] (->> hand
+                      (match-n-of-a-kind 3)
+                      first)]
+    [(*rankings* :three-of-a-kind) v]))
+
+(defn match-straight [hand]
+  (let [ace-low (fn [{:keys [value suit]}]
+                  {:suit suit :value (if (= value 14) 1 value)})]
+    (or (match-straight-impl hand)
+        (match-straight-impl (map ace-low hand)))))
+
+(defn match-flush [hand]
+  (apply = (map :suit hand)))
+
+(defn match-full-house [hand]
+  nil)
+
+(defn match-four-of-a-kind [hand]
+  (if-let [[_ v] (->> hand
+                      (match-n-of-a-kind 4)
+                      first)]
+    [(*rankings* :four-of-a-kind) [v]]))
+
+(defn match-straight-flush [hand]
+  (if-let [[_ high-card] (and (match-flush hand) (match-straight hand))]
+    [(*rankings* :straight-flush) high-card]))
+
+
+(defn value
+  "Returns the value of the hand as a vector containing:
+    1) The value associated with the type of hand (see *rankings*)
+    2) A vector of card-values.
+  For example, a full-house of fives over jacks would have the
+  value [80 [5 11]], while a pair of threes would have [30 [3]].
+  Values are represented this way to allow the natural sorting
+  order of clojure vectors to properly rank hands."
+  [hand]
+  (let [matchers [match-straight-flush
+                  match-four-of-a-kind
+                  match-full-house
+                  match-flush
+                  match-straight
+                  match-three-of-a-kind
+                  match-two-pair
+                  match-pair
+                  match-high-card
+                  ]
+        matches (remove nil? (for [m matchers] (m hand)))]
+    (first matches)))
--- poker/test/poker/test/core.clj
+++ poker/test/poker/test/core.clj
@@ -2,11 +2,63 @@
   (:use [poker [core :as p]])
   (:use [clojure.test]))
 
+
+(def h p/parse-hand)
+
 (deftest test-parse
-  (is (= [[:c 2] [:d 3] [:h 4] [:s 5] [:c 6]]
-         (p/parse-hand "2c 3d 4h 5s 6c")))
-  (is (= [[:c 14] [:d 13] [:h 12] [:s 11] [:c 10]]
-         (p/parse-hand "Ac Kd Qh Js 10c")))
+  (is (= [{:suit :c, :value 2}
+          {:suit :d, :value 3}
+          {:suit :h, :value 4}
+          {:suit :s, :value 5}
+          {:suit :c, :value 6}]
+         (h "2c 3d 4h 5s 6c")))
+  
+  (is (= [{:suit :c, :value 14}
+          {:suit :d, :value 13}
+          {:suit :h, :value 12}
+          {:suit :s, :value 11}
+          {:suit :c, :value 10}]
+         (h "Ac Kd Qh Js 10c")))
+  )
+
+(deftest ranking
+  (is (= [[3 5] [1 14] [1 13]]
+         (p/ranked-groups (h "5s Ah 5c Kh 5d")))))
+
+
+(deftest test-high-card
+  (is (= [9] (p/match-high-card (h "2c 8s 9c 6h 5d")))))
+
+(deftest test-pair
+  (is (= [12] (p/match-pair (h "Qh Ad Ks Qd 3c")))))
+
+(deftest test-two-pair
+  (is (= [14 7] (p/match-two-pair (h "7c Ad 7h 9s 7d")))))
+
+(deftest test-tree-of-a-kind
+  (is (= [6] (p/match-three-of-a-kind (h "As 6c 6d 7h 6h")))))
+
+(deftest test-straight
+  (is (= [11] (p/match-straight (h "Jc 7c 9s 8d 10c"))))
+  (is (= [14] (p/match-straight (h "10s Ah Qs Kc Js"))))
+  (is (= [5]  (p/match-straight (h "Ah 2d 5c 3d 4s"))))
+  )
+
+(deftest test-flush
+  (is (= [12] (p/match-flush (h "5s 6s 2s Qs 10s")))))
+
+(deftest test-full-house
+  (is (= [5 9] (p/match-full-house (h "9d 5d 5s 9h 5c"))))
+  (is (= [9 5] (p/match-full-house (h "9d 5d 5s 9h 9c"))))
+  )
+
+(deftest test-four-of-a-kind
+  (is (= [2] (p/match-four-of-a-kind (h "2c Js 2d 2h 2s")))))
+
+(deftest test-straight-flush
+  (is (= [11] (p/match-straight (h "Jc 7c 9c 8c 10c"))))
+  (is (= [14] (p/match-straight (h "10s As Qs Ks Js"))))
+  (is (= [5]  (p/match-straight (h "Ad 2d 5d 3d 4d"))))
   )
 
 
