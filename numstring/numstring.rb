5a5a49c62b36b4a867c415097255904cde7797e0
Chris Perkins <chrisperkins99@gmail.com>
2012-06-02 20:20:58 -0400

    Implement format_currency for English.
    All tests are passing now, and have pretty good coverage, I think.
    Next step is to refactor a bit, and to try to validate the architecture
    for enabling pluggable support for other languages.

--- numstring/numstring.rb
+++ numstring/numstring.rb
@@ -1,13 +1,140 @@
 # numstring.rb
 
+#require 'pry'
+
 # Adds methods to the built-in Numeric class:
 #  to_nl_string: returns a natural-language representation of the
 #                number. eg: 23.45 => "twenty-tree point four-five"
 #  to_currency_string: returns a natural-language representation of
 #                      the number as dollars and cents.
 
+module NumberFormat
+
+  private
+
+  NumberNames = {}
+
+  NumberNames['en'] = {
+
+    :negative => 'minus ',
+
+    :and => ' and ',
+
+    :special => {
+      0 => "zero",
+      1 => "one",
+      2 => "two",
+      3 => "three",
+      4 => "four",
+      5 => "five",
+      6 => "six",
+      7 => "seven",
+      8 => "eight",
+      9 => "nine",
+      10 => "ten",
+      11 => "eleven",
+      12 => "twelve",
+      13 => "thirteen",
+      14 => "fourteen",
+      15 => "fifteen",
+      16 => "sixteen",
+      17 => "seventeen",
+      18 => "eighteen",
+      19 => "nineteen",
+      20 => "twenty",
+      30 => "thirty",
+      40 => "forty",
+      50 => "fifty",
+      60 => "sixty",
+      70 => "seventy",
+      80 => "eighty",
+      90 => "ninety",
+    },
+
+    :steps => [
+      # Do not use quadrillion, quintillion, etc. - just use "one
+      # thousand trillion", "ten trillion trillion" etc. ad infinitum
+      [1e12.to_i, "trillion"],
+      [1e9.to_i, "billion"],
+      [1e6.to_i, "million"],
+      [1e3.to_i, "thousand"],
+      [1e2.to_i, "hundred"],
+    ]
+
+  }
+
+  def decompose(n, lang='en')
+    # Decompose a number into an array of pairs of numbers and names
+    # eg: 923000357 => [[923, "million"], [0, "thousand"], [3,
+    # "hundred"], [57, ""]]
+    n = n.to_i
+    steps = NumberNames[lang][:steps]
+    steps.each {|step,name|
+      if n >= step
+        return [[n / step, name]] + decompose(n % step)
+      end
+    }
+    []
+  end
+
+  def pluralize(unit, n)
+    # TODO - make this localizable
+    return unit + (n.abs == 1 ? '' : 's')
+  end
+
+  def format_currency(n, lang='en')
+    fmt = NumberNames[lang]
+    
+    # Deal with negatives here, to avoid things like "minus one dollar
+    # and minus three cents"
+    result = n < 0 ? fmt[:negative] : ''
+    n = n.abs
+
+    cents = ((n % 1.0) * 100).round.to_i
+    dollars = n.floor.to_i
+
+    # Determine whether to include the dollars part and/or the cents
+    # part. Make sure to return "zero dollars", not "zero cents" for 0.
+    include_dollars = n.zero? || !dollars.zero?
+    include_cents = !cents.zero?
+
+    result += format_number(dollars) + ' ' + pluralize('dollar', dollars) if include_dollars
+    result += fmt[:and] if (include_dollars && include_cents)
+    result += format_number(cents) + ' ' + pluralize('cent', cents) if include_cents
+
+    result
+  end
+
+  def format_number(n, lang='en')
+    n = n.to_i
+    fmt = NumberNames[lang]
+
+    # Negatives:
+    return fmt[:negative] + format_number(-n) if n < 0
+
+    # Special cases:
+    s = fmt[:special][n]
+    return s unless s.nil?
+
+    # Numbers up to 100
+    if n < 100
+      return fmt[:special][n/10*10] + '-' + fmt[:special][n % 10]
+    end
+
+    # All other cases
+    result = decompose(n).map {|num, name|
+      format_number(num) + " " + name unless num.zero?
+    }.reject(&:nil?).join(' ')
+    rem = n % 100
+    result += ' ' + format_number(rem) unless rem.zero?
+    result
+  end
+  
+end
 
 class Numeric
+
+  include NumberFormat
 
   def to_nl_string(lang = 'en')
     # Convert to natural language string
@@ -18,7 +145,6 @@ class Numeric
     # Convert to a natural language string representing a dollar
     # amout.
     # Note: Numbers are rounded to the nearest cent.
-    "fiddy cent"
+    format_currency(self)
   end
-
 end
--- numstring/numstring_test.rb
+++ numstring/numstring_test.rb
@@ -2,6 +2,7 @@ require './numstring'
 require 'test/unit'
 
 class NumstringTest < Test::Unit::TestCase
+
   def test_to_currency_string
     [
      # Simple cases:
@@ -36,7 +37,12 @@ class NumstringTest < Test::Unit::TestCase
      # Cents only
      0.01, "one cent",
      0.02, "two cents",
-     0.99, "ninety nine cents",
+     0.99, "ninety-nine cents",
+     
+     # I have defined the method to discard fractional cents (currency
+     # traders may cringe at this)
+     0.144997, "fourteen cents",
+     0.145001, "fifteen cents",
 
      # Negative numbers
      -1, "minus one dollar",
@@ -49,14 +55,20 @@ class NumstringTest < Test::Unit::TestCase
      100.02, "one hundred dollars and two cents",
      101, "one hundred one dollars",
      101.01, "one hundred one dollars and one cent",
-     199.99, "one hundred ninety-nine dollars and nintety-nine cents",
+     199.99, "one hundred ninety-nine dollars and ninety-nine cents",
      765.00, "seven hundred sixty-five dollars",
 
      # And some big, complex ones
      2523.04,  "two thousand five hundred twenty-three dollars and four cents",
      1000000, "one million dollars", # insert Dr. Evil joke here
      923002388.19, "nine hundred twenty-three million two thousand three hundred eighty-eight dollars and nineteen cents",
-     1000000001, "one billion and one dollars",
+     1000000001, "one billion one dollars",
+     1234567890, 'one billion two hundred thirty-four million five hundred sixty-seven thousand eight hundred ninety dollars',
+     123456789000, 'one hundred twenty-three billion four hundred fifty-six million seven hundred eighty-nine thousand dollars',
+     12345678900000, 'twelve trillion three hundred forty-five billion six hundred seventy-eight million nine hundred thousand dollars',
+     1234567890000000, 'one thousand two hundred thirty-four trillion five hundred sixty-seven billion eight hundred ninety million dollars',
+     123456789000000000, 'one hundred twenty-three thousand four hundred fifty-six trillion seven hundred eighty-nine billion dollars',
+     10000000000000000000000000000001, 'ten million trillion trillion one dollars', # sounds a bit funny without "and" before the "one"... oh well
      
      
     ].each_slice(2) do |n,s|
