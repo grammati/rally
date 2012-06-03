4259f5735eeef8e06030fffb0e0a5a24ca7f5970
Chris Perkins <chrisperkins99@gmail.com>
2012-06-03 09:52:33 -0400

    Basic tests are passing for "safe" input - 1, 9, 25, etc.
    Just need to correct for other inputs now, and possibly format the output a bit.

--- spiral/spiral.py
+++ spiral/spiral.py
@@ -7,11 +7,64 @@ from StringIO import StringIO
 class Spiraler:
 
     def __init__(self, n):
-        assert n >= 0, "n must not be negative"
-        self.n = n
+        assert n > 0, "n must be one or more"
+        
+        # limit is the largest number to be printed
+        self.limit = n - 1
+
+        # The "order" is the number of cells in each row or column, which is
+        # always and odd number.
+        # A spiral of order N contains a spiral or order N-2 inside it.
+        self.order = int( math.ceil( math.sqrt(n) ) )
+
+        # Set self.n to the total number of cells in this spiral:
+        self.n = self.order ** 2
+
+        # Delegate to another instance responsible for creating the inner spiral
+        # - that is, the spiral of order self.order - 2.
+        if self.order >= 3:
+            self.inner = Spiraler((self.order - 2) ** 2)
+        else:
+            self.inner = None
+
+        # For convenience, precalculate the lowest number in the "outer shell".
+        self.first = (self.order - 2) ** 2
+
+    def row(self, rownum):
+        "Returns an iterator of the numbers that constitute the given row (zero-based)."
+        if rownum == 0:
+            # first row is the ascending sequence of self.order
+            # numbers ending at self.n
+            for i in range(self.n - self.order, self.n):
+                yield i
+        elif rownum == self.order - 1:
+            # last row is the descending sequence of self.order
+            # numbers from (self.n - self.order - (self.order - 1))
+            start = (self.n 
+                     - self.order # first row
+                     - (self.order - 1)) # first column - top cell
+            for i in range(start, start - self.order, -1):
+                yield i
+        else:
+            # Other rows: number in the first column, then delegate to inner,
+            # then number in last column.
+            yield self.n - self.order - rownum
+            if self.inner:
+                for i in self.inner.row(rownum - 1):
+                    yield i
+            yield self.first + rownum - 1
+
+    def rows(self):
+        "Returns an iterator over the rows, where each row is itself an iterator over numbers."
+        for rownum in range(0, self.order):
+            yield self.row(rownum)
 
     def write_to(self, writer):
-        writer.write("%d\n" % self.n)
+        for row in self.rows():
+            for number in row:
+                writer.write("%d " % number)
+            writer.write("\n")
+
 
 def string(n):
     "Write a number-spiral of the given size to a string, and return it."
--- spiral/spiral_test.py
+++ spiral/spiral_test.py
@@ -3,13 +3,19 @@ import spiral
 
 class SpiralTest(unittest.TestCase):
     def test_spiral(self):
-        self.assertEqual("0\n", spiral.string(0))
-        self.assertEqual("""20 21 21 23 24
-19 6 7 8 9
-18 5 0 1 10
-17 4 3 2 11
-16 15 14 13 12
-""", spiral.string(24))
+        self.assertEqual("0 \n", spiral.string(1))
+
+        self.assertEqual("""6 7 8 
+5 0 1 
+4 3 2 
+""", spiral.string(9))
+
+        self.assertEqual("""20 21 22 23 24 
+19 6 7 8 9 
+18 5 0 1 10 
+17 4 3 2 11 
+16 15 14 13 12 
+""", spiral.string(25))
 
 
 if __name__ == '__main__':
